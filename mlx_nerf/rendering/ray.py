b2f013b618861e21f0f28dba11a6f03e9319ddfd
piljoong-jeong <43630935+piljoong-jeong@users.noreply.github.com>
2024-02-23 23:20:09 +0900

    impl: NDC ray transform

--- mlx_nerf/rendering/ray.py
+++ mlx_nerf/rendering/ray.py
@@ -31,3 +31,38 @@ def get_rays(H: int, W: int, K: Union[onp.ndarray, mx.array], c2w: Union[onp.nda
 
     # TODO: cast? at least we don't have to store this as onp.ndarray
     return rays_o, rays_d
+
+# TODO: why `near` only?
+# NOTE: `rays_o` to `near` plane, endpoint is `1` (in NDC)
+def ndc_rays(H, W, focal, near, rays_o, rays_d):
+
+    # NOTE: shift `rays_o` to near plane (z = -`near`)
+    # NOTE: see last paragraph in Appendix. C
+    o_z = rays_o[..., 2]
+    d_z = rays_d[..., 2]
+    # NOTE: o_n = o + t_n*d, for t_n = -(n + o_z) / d_z
+    t_n = -(near + o_z) / d_z
+    rays_o = rays_o + t_n[..., None] * rays_d
+
+    # NOTE: map `rays_o` & `rays_d` to NDC space
+    o_x = rays_o[..., 0]
+    o_y = rays_o[..., 1]
+    o_z = rays_o[..., 2]
+    
+    # NOTE: eq. (25)
+    o0 = (-focal / (0.5*W)) * (o_x / o_z)
+    o1 = (-focal / (0.5*H)) * (o_y / o_z)
+    o2 = (1.0 + 2.0*near / o_z) 
+
+    # NOTE: eq. (26)
+    d_x = rays_d[..., 0]
+    d_y = rays_d[..., 1]
+    d_z = rays_d[..., 2]
+    d0 = (-focal / (0.5*W)) * (d_x/d_z - o_x/o_z)
+    d1 = (-focal / (0.5*H)) * (d_y/d_z - o_y/o_z)
+    d2 = -2.0 * near * (1.0 / o_z)
+
+    return (
+        (rays_o := mx.stack([o0, o1, o2], axis=-1)), 
+        (rays_d := mx.stack([d0, d1, d2], axis=-1))
+    )
