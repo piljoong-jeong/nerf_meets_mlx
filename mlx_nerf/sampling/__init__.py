fa36db1cedfdece796628133a111b3deb840426b
piljoong-jeong <43630935+piljoong-jeong@users.noreply.github.com>
2024-02-28 20:56:15 +0900

    snapshot: use `numpy.searchsorted`

--- mlx_nerf/sampling/__init__.py
+++ mlx_nerf/sampling/__init__.py
@@ -1,3 +1,4 @@
+import numpy as onp
 import mlx.core as mx
 
 __all__ = ["add_noise_z"]
@@ -26,3 +27,40 @@ def add_noise_z(
     z_vals = lower + (upper - lower) * t_rand
 
     return z_vals
+
+def sample_from_inverse_cdf(
+    z_vals, 
+    weights, 
+    n_importance_samples, 
+    eps = 1e-5, 
+    is_stratified_sampling=False, 
+):
+    z_vals_mid = (z_vals[..., 1:] + z_vals[..., :-1]) / 2
+    weights = weights[..., 1:-1] + eps # [B, n_samples-1]
+    weights_sum = mx.sum(weights, axis=-1, keepdims=True)
+
+    # NOTE: PDF is proportional to `weights(=transmittance)`, from geometric probability's perspective
+    pdf = weights / weights_sum
+    cdf = mx.cumsum(pdf, axis=-1)
+    cdf = mx.minimum(mx.ones_like(cdf), cdf) # NOTE: clip
+    cdf = mx.concatenate(
+        [
+            mx.zeros_like(cdf[..., 0]), 
+            cdf
+        ], axis=-1
+    ) # [B, n_samples]
+
+    # NOTE: similar with `t_vals` seen in samplers, but named as `u_vals` to indicate they're importance-sampled ones
+    u_vals = None
+    if is_stratified_sampling:
+        u_vals = mx.linspace(0.0, 1.0, num=n_importance_samples)
+        u_vals = mx.repeat(u_vals[None, ...], repeats=cdf.shape[0], axis=0) # TODO: double-check
+
+    else: # NOTE: uniform sampling
+        u_vals = mx.random.normal(
+            tuple(list(cdf.shape[:-1]) + list(n_importance_samples))
+        )
+
+    # TODO: check if it's OK to use numpy's implementation; I guess so as no grad would be generated, but just to make sure...
+    inds = onp.searchsorted(onp.array(cdf, copy=False), u_vals, side="right")
+
