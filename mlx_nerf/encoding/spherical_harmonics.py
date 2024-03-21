66bb8f1734da625eb860b76094d28236a6536403
piljoong-jeong <43630935+piljoong-jeong@users.noreply.github.com>
2024-03-21 22:31:25 +0900

    wrote all SHs

--- mlx_nerf/encoding/spherical_harmonics.py
+++ mlx_nerf/encoding/spherical_harmonics.py
@@ -55,26 +55,41 @@ class SphericalHarmonicsEncoding(Encoding):
 
         out_encoded = mx.zeros((*in_dirs.shape[:-1], self.get_out_dim()), dtype=in_dirs.dtype)
 
-        # NOTE: by evaluating https://en.wikipedia.org/wiki/Table_of_spherical_harmonics#Real_spherical_harmonics
+        # NOTE: all the coefficients below are constants, 
+        # NOTE: appeared during evaluation of https://en.wikipedia.org/wiki/Table_of_spherical_harmonics#Real_spherical_harmonics
         # NOTE: here, `r=1` as `in_dirs` is unit vector set
         level = self.n_degrees
-        if level >= 0: # NOTE: for readability
-            out_encoded[..., 0] = 0.28209479177387814
+        if level >= 0:
+            # NOTE: 1/2 * sqrt(1 / PI)
+            out_encoded[...,  0] = 0.28209479177387814 
         if level >= 1:
-            out_encoded[..., 1] = 0.4886025119029199 * y
-            out_encoded[..., 2] = 0.4886025119029199 * z
-            out_encoded[..., 3] = 0.4886025119029199 * x
+            # NOTE: sqrt(3 / 4*PI)
+            out_encoded[...,  1] = 0.4886025119029199 * y 
+            out_encoded[...,  2] = 0.4886025119029199 * z
+            out_encoded[...,  3] = 0.4886025119029199 * x
         if level >= 2:
-            out_encoded[..., 4] = 1.0925484305920792 * xy
-            out_encoded[..., 5] = 1.0925484305920792 * yz
-            out_encoded[..., 6] = 0.9461746957575601 * zz - 0.31539156525251999
-            out_encoded[..., 7] = 1.0925484305920792 * xz
-            out_encoded[..., 8] = 0.5462742152960396 * (xx - yy)
+            out_encoded[...,  4] = 1.0925484305920792 * xy
+            out_encoded[...,  5] = 1.0925484305920792 * yz
+            out_encoded[...,  6] = 0.9461746957575601 * zz - 0.31539156525251999
+            out_encoded[...,  7] = 1.0925484305920792 * xz
+            out_encoded[...,  8] = 0.5462742152960396 * (xx - yy)
         if level >= 3:
-            # TODO
-            pass
+            out_encoded[...,  9] = 0.5900435899266435 * y * (3 * xx - yy)
+            out_encoded[..., 10] = 2.890611442640554 * xy * z
+            out_encoded[..., 11] = 0.4570457994644658 * y * (5 * zz - 1) # NOTE: 4zz-xx-yy = 5zz-(xx+yy+zz) = 5zz - 1
+            out_encoded[..., 12] = 0.3731763325901154 * z * (5 * zz - 3) # NOTE: 2zz-3xx-3yy = 5zz-3(zz+xx+yy) = 5zz - 3
+            out_encoded[..., 13] = 0.4570457994644658 * x * (5 * zz - 1) 
+            out_encoded[..., 14] = 1.445305721320277 * z * (xx - yy)
+            out_encoded[..., 15] = 0.5900435899266435 * x * (xx - 3 * yy)
         if level >= 4:
-            # TODO
-            pass
+            out_encoded[..., 16] = 2.5033429417967046 * xy * (xx - yy)
+            out_encoded[..., 17] = 1.7701307697799304 * yz * (3 * xx - yy)
+            out_encoded[..., 18] = 0.9461746957575601 * xy * (7 * zz - 1)
+            out_encoded[..., 19] = 0.6690465435572892 * yz * (7 * zz - 3)
+            out_encoded[..., 20] = 0.10578554691520431 * (35 * zz * zz - 30 * zz + 3)
+            out_encoded[..., 21] = 0.6690465435572892 * xz * (7 * zz - 3)
+            out_encoded[..., 22] = 0.47308734787878004 * (xx - yy) * (7 * zz - 1)
+            out_encoded[..., 23] = 1.7701307697799304 * xz * (xx - 3 * yy)
+            out_encoded[..., 24] = 0.6258357354491761 * (xx * (xx - 3 * yy) - yy * (3 * xx - yy))
 
-        raise NotImplementedError
+        return out_encoded
